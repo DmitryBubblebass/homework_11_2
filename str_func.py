ff7fa33f0b636f9f926505fe1a141efc7e17b9b4
Dmitry BubbleBass <dmitry.bubblebass@gmail.com>
2023-07-20 14:05:03 +0300

    add def makes_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,14 @@
-def make_upper(message):
+def makes_upper(message):
+    """
+     Делает все буквы заглавными
+    """
     up_message = message.upper()
     return up_message
 
 
-
+def makes_title(message):
+    """
+    Делает все ПЕРВЫЕ буквы заглавными
+    """
+    title_message = message.title()
+    return title_message
